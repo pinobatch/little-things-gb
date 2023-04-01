69d6aa3cc950a9b0e53154b87a608f55165fb971
Damian Yerrick <pino@pineight.com>
2023-04-01 15:31:03 -0400

    add the menu

--- afterglow/mk.sh
+++ afterglow/mk.sh
@@ -4,14 +4,15 @@ set -e
 mkdir -p obj/gb
 python3 tools/vwfbuild.py tilesets/vwf7.png obj/gb/vwf7.z80
 rgbgfx -o obj/gb/chr16.2bpp -Zc embedded tilesets/chr.png
-rgbasm -Ho obj/gb/main.o src/main.z80
-rgbasm -Ho obj/gb/vwfdraw.o src/vwfdraw.z80
-rgbasm -Ho obj/gb/vwflabels.o src/vwflabels.z80
-rgbasm -Ho obj/gb/vwf7.o obj/gb/vwf7.z80
-rgbasm -Ho obj/gb/pads.o src/pads.z80
-rgbasm -Ho obj/gb/ppuclear.o src/ppuclear.z80
-rgbasm -Ho obj/gb/sgb.o src/sgb.z80
+rgbasm -ho obj/gb/main.o src/main.z80
+rgbasm -ho obj/gb/vwfdraw.o src/vwfdraw.z80
+rgbasm -ho obj/gb/vwflabels.o src/vwflabels.z80
+rgbasm -ho obj/gb/vwf7.o obj/gb/vwf7.z80
+rgbasm -ho obj/gb/pads.o src/pads.z80
+rgbasm -ho obj/gb/ppuclear.o src/ppuclear.z80
+rgbasm -ho obj/gb/sgb.o src/sgb.z80
+rgbasm -ho obj/gb/bcd.o src/bcd.z80
 rgblink -dto afterglow.gb -n afterglow.sym \
   obj/gb/main.o obj/gb/vwfdraw.o obj/gb/vwflabels.o obj/gb/vwf7.o \
-  obj/gb/pads.o obj/gb/sgb.o obj/gb/ppuclear.o
-rgbfix -jvsl 0x33 -t 'AFTERGLOW' afterglow.gb
+  obj/gb/pads.o obj/gb/sgb.o obj/gb/ppuclear.o obj/gb/bcd.o
+rgbfix -jvscl 0x33 -t 'AFTERGLOW' afterglow.gb
